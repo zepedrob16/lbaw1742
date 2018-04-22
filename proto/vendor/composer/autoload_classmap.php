5ca3947991c06d26270ae0fb5d96a7a54a784de4
zepedrob16 <ze_pedro_borges@hotmail.com>
2018-04-22 19:17:46 +0100

    dependencias

--- proto/vendor/composer/autoload_classmap.php
+++ proto/vendor/composer/autoload_classmap.php
@@ -30,6 +30,7 @@ return array(
     'App\\Providers\\BroadcastServiceProvider' => $baseDir . '/app/Providers/BroadcastServiceProvider.php',
     'App\\Providers\\EventServiceProvider' => $baseDir . '/app/Providers/EventServiceProvider.php',
     'App\\Providers\\RouteServiceProvider' => $baseDir . '/app/Providers/RouteServiceProvider.php',
+    'App\\Text_Post' => $baseDir . '/app/Text_Post.php',
     'App\\User' => $baseDir . '/app/User.php',
     'ArithmeticError' => $vendorDir . '/symfony/polyfill-php70/Resources/stubs/ArithmeticError.php',
     'AssertionError' => $vendorDir . '/symfony/polyfill-php70/Resources/stubs/AssertionError.php',
--- proto/vendor/composer/autoload_static.php
+++ proto/vendor/composer/autoload_static.php
@@ -380,6 +380,7 @@ class ComposerStaticInit4453afec5190fa1d6d5b8bb9515e3340
         'App\\Providers\\BroadcastServiceProvider' => __DIR__ . '/../..' . '/app/Providers/BroadcastServiceProvider.php',
         'App\\Providers\\EventServiceProvider' => __DIR__ . '/../..' . '/app/Providers/EventServiceProvider.php',
         'App\\Providers\\RouteServiceProvider' => __DIR__ . '/../..' . '/app/Providers/RouteServiceProvider.php',
+        'App\\Text_Post' => __DIR__ . '/../..' . '/app/Text_Post.php',
         'App\\User' => __DIR__ . '/../..' . '/app/User.php',
         'ArithmeticError' => __DIR__ . '/..' . '/symfony/polyfill-php70/Resources/stubs/ArithmeticError.php',
         'AssertionError' => __DIR__ . '/..' . '/symfony/polyfill-php70/Resources/stubs/AssertionError.php',
