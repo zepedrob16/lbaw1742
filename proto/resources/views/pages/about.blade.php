26cd83e70f60968fe4b1f9e2575644a03e14fffc
zepedrob16 <ze_pedro_borges@hotmail.com>
2018-05-30 02:52:40 +0100

    about

--- proto/resources/views/pages/about.blade.php
+++ proto/resources/views/pages/about.blade.php
@@ -6,7 +6,7 @@
  <div class="container">
 
  	<h1 class="my-4"> About Us </h1>
- 	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed volutpat, purus ut scelerisque eleifend, lectus nibh vehicula ante, sit amet viverra erat felis vel tellus. Integer tellus lectus, aliquet ut tristique ut, aliquam non velit. Aliquam at sodales nulla, ut congue mi. Donec dignissim mattis nunc a congue. Proin nec metus nec mauris posuere facilisis. Suspendisse potenti. Aenean vitae elementum massa. Donec eu porta neque. Maecenas elementum, leo ut scelerisque tristique, mi velit egestas ex, quis fermentum lacus velit eu velit. Etiam eu elementum erat. Sed tristique fermentum consectetur. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In hac habitasse platea dictumst. Aenean blandit mollis ipsum congue lobortis. </p>
+ 	<p>Este projeto foi realizado no âmbito da unidade curricular de Laboratório de Base de Dados e Aplicações Web. Foi feito com muito esforço, apesar de não ter tido muito êxito, e causou muitas noites sem dormir a estes quatro nabos aqui abaixo.</p>
  	
  	<div class ="row">
  		<div class="col-lg-12 mb-4">
